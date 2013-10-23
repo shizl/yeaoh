0b7bb5f2ab1bd361cd11d711b8fe5b1a6cfd2673
Yeaoh Media Inc <support@yeaoh.com>
2013-10-23 12:53:20 +0800

    add roboot.txt and tmp folder

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,14 +1,14 @@
 #Repository variables-----------------------------
 set :scm,         :git
 #set :deploy_via, :remote_cache
-set :repository,  "https://yeaoh:yeaoh1@bitbucket.org/yeaoh/yeaoh.comenglish.git"
+set :repository,  "https://yeaoh:yeaoh1@bitbucket.org/yeaoh/yeaoh.comen.git"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
 
 
 set  :keep_releases,  3
-set :app_symlinks, ["/yeaoh/sites/default"]
+set :app_symlinks, ["/yeaoh/sites/default , /yeaoh/tmp"]
 #set :app_shared_dirs, ["/yeaoh/sites/default"]
-set :app_shared_files, ["/yeaoh/sites/default/settings.php", "/yeaoh/.htaccess"]
+set :app_shared_files, ["/yeaoh/.htaccess"]
 #set :app_root_dir, "#{current_path}/public"
 
 
