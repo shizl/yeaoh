48ee22f7ef24bb67b6ebf721c089f99e371e357f
Gilbert MONTCHO <gilbert@Gilberts-Mac-mini.local>
2013-10-28 18:07:43 +0800

    update thme

--- yeaoh/sites/all/themes/yeaoh_website/html.tpl.php
+++ yeaoh/sites/all/themes/yeaoh_website/html.tpl.php
@@ -11,18 +11,33 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  
-  	<script>
-	jQuery(document).ready(function(){
-	
-	   jQuery('.fancybox').fancybox(); 
-	     
-	});
-	</script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34926348-1']);
+  _gaq.push(['_setDomainName', 'yeaoh.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-<?php print $page_top ;?>
-<?php print $page ;?>
-<?php print $page_bottom ;?>
+<!-- Facebook Social Plugin Javascript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=231262563644329";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  
 </body>
 </html>
