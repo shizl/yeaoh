7c37d936f4da93b33a46046a0b93768d0b288d02
Gilbert M <poidstotal@gmail.com>
2013-11-17 23:49:42 +0800

    update google analytic

--- yeaoh/sites/all/themes/yeaoh_website/html.tpl.php
+++ yeaoh/sites/all/themes/yeaoh_website/html.tpl.php
@@ -11,21 +11,26 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <!--google analytic js start-->
 <script type="text/javascript">
+
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-34926348-1']);
   _gaq.push(['_setDomainName', 'yeaoh.com']);
+  _gaq.push(['_setAllowLinker', true]);
   _gaq.push(['_trackPageview']);
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
 </script>
+  <!--google analytic js end -->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-<!-- Facebook Social Plugin Javascript -->
+<!-- Facebook Social Plugin Javascript start -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -33,8 +38,11 @@
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=231262563644329";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
+}(document, 'script', 'facebook-jssdk'));
+</script>
+<!-- Facebook Social Plugin Javascript end-->
+  
+  
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
