b2cb8e18033d42466a8cba664958a7d70e192787
Martin You <martin.you@yeaoh.net>
2014-05-28 09:12:48 +0800

    just testing

--- yeaoh/sites/all/themes/myweb/templates/views-view-unformatted--meet-your-team--page-1.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
-<?php endif; ?>
-<div class="team-box clearfix">
-<?php foreach ($rows as $id => $row): ?>
-    <?php print $row; ?>
-<?php endforeach; ?>
-</div>
