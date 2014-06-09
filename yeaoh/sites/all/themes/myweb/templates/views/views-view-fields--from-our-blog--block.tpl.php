9d845ff95478c28e0b0b81da0dd1b6204b543267
neo <kaiyuan.an@yeaoh.net>
2014-06-09 16:26:19 +0800

    tpl

--- yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--from-our-blog--block.tpl.php
+++ yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--from-our-blog--block.tpl.php
@@ -9,9 +9,6 @@
                             <span><?php print $fields['created']->content;?><span><?php print $fields['created_1']->content;?></span></span>
                             </div>
                             <div class="blog-text">
-                              <div class="meta">
-                              <span><i class="fa fa-user"></i><?php print $fields['name_1']->content;?></span> <span><a href="#"><i class="fa fa-comment"></i> 25 Comments</a></span>
-                                </div>
                                 <h3><?php print $fields['title']->content;?></h3>
 <?php print $fields['body']->content;?>
 <div class="my-btn bordered btn-small"> <?php print $fields['title_1']->content;?></div>
