d1b7848a792c25052e2aeb7826d069da610eb677
neo <kaiyuan.an@yeaoh.net>
2014-06-10 20:12:15 +0800

    meni

--- yeaoh/sites/all/themes/myweb/templates/page/page.tpl.php
+++ yeaoh/sites/all/themes/myweb/templates/page/page.tpl.php
@@ -1,13 +1,32 @@
+22222222222222222222222222
 <div id="page_wrapper"><!-- start page_wrapper -->
   <div id="center"><!-- start cneter -->
+<script language="javascript">
+var m=0;
+function show_box(sid){
+  //manin menu show 
+  if(jQuery('.menu_des').css('display')=='none'){
+    jQuery('.all_menu').css('margin-left',(1-sid)*990);
+  }else{
+    jQuery('.all_menu').animate({marginLeft: (1-sid)*990}, 500);
+  }
+  if(m==sid){
+    jQuery('.menu_des').slideUp();
+    m=0;
+  }else{
+    m=sid;
+    jQuery('.menu_des').slideDown();
+  }
 
-    <div id="page_top"><!-- start page_top -->
+}
+</script>
+     <div id="page_top"><!-- start page_top -->
       <div class="header">
         <div id="toplink"><!-- stat toplink-->
-          <?php if($page['top_link']): ?>
-          <?php  print render ($page['top_link']) ;?>
-          <?php endif ; ?>
-        </div><!-- end toplink-->
+           <?php if($page['top_link']): ?>
+            <?php  print render ($page['top_link']) ;?>
+           <?php endif ; ?>
+         </div><!-- end toplink-->
         <?php if ($logo): ?> 
         <!-- start logo wrapper -->
         <div id="logo-wrapper">
@@ -18,7 +37,7 @@
         <?php endif; ?>
 
         <div id="main-menu"><!-- start main_menu -->
-          <?php if ($main_menu ): ?>
+        <?php if ($main_menu ): ?>
 <?php 
 function show_menu($mlid=0,$lid=0,$mname='root_menu'){
   $rootmenu=db_query("SELECT `mlid`,`link_path`,`link_title`,'options' FROM `menu_links` WHERE `menu_name`='main-menu' AND `plid` ='$mlid' AND `language`='".(!empty($_REQUEST['language'])?$_REQUEST['language']:'en')."' ORDER BY `weight` ASC");
@@ -55,58 +74,57 @@ echo show_menu();
 
 ?>
 
+         <?php endif; ?>
+         </div><!-- main menu end -->
+      </div>
+      <div class="head_bottom_bg"></div> </div> <!-- page top end-->
+     <!-- start centermain -->
+
+                        <div id="main_content">
+           <div class="main_content">
+
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title">
+          <?php print $title; ?>
+        </h1>
       <?php endif; ?>
-    </div><!-- main menu end -->
-  </div>
-  <div class="head_bottom_bg"></div> </div> <!-- page top end-->
-<!-- start centermain -->
-<div id="main_content">
-  <div class="main_content">
 
-    <?php if ($title): ?>
-    <h1 class="title" id="page-title">
-      <?php print $title; ?>
-    </h1>
-    <?php endif; ?>
-    <?php  if($tabs): ?>
-    <div class="tabs"><?php  print render($tabs) ;?></div>
-    <?php endif ;?> 
-    <div class="s-cotnent">
+             <?php  if($tabs): ?>
+          <div class="tabs"><?php  print render($tabs) ;?></div>
+        <?php endif ;?> 
+        <div class="s-cotnent"> <?php print render($page['content']); ?></div>
+        <?php  if($page['left']): ?>
+          <?php  print render($page['left']) ; ?>
+        <?php endif ;?>
+         </div>
+                                    <div class="protfolio">
+         <?php if($page['home_column3']): ?>
+           <div class="pro_content clearfix"><?php print render($page['home_column3']) ;?></div>
+         <?php endif ; ?> 	   
+      </div>
+      </div>
 
-
-      <?php print render($page['content']); ?>
-    </div>
-    <?php  if($page['left']): ?>
-    <?php  print render($page['left']) ; ?>
-    <?php endif ;?>
-  </div>
-  <div class="protfolio">
-    <?php if($page['home_column3']): ?>
-    <div class="pro_content clearfix"><?php print render($page['home_column3']) ;?></div>
-    <?php endif ; ?> 	   
-  </div>
-</div>
-
-<div class="footer_main">
-  <?php if($page['foot_main']): ?>
-  <div class="foot_content clearfix">  <?php  print render ($page['foot_main']) ;?> </div>
-  <?php endif ; ?>	
-</div>
+      <div class="footer_main">
+        <?php if($page['foot_main']): ?>
+        <div class="foot_content">  <?php  print render ($page['foot_main']) ;?> </div>
+         <?php endif ; ?>	
+      </div>
 
     </div><!-- end contermain -->
 
     <!-- start footer -->
     <div id="footer">
-<div class="footer-bottom">
-  <?php if($page['footer']): ?>
-  <?php  print render ($page['footer']) ;?>
-  <?php endif ; ?></div>
+      <div class="footer-bottom">
+         <?php if($page['footer']): ?>
+          <?php  print render ($page['footer']) ;?>
+         <?php endif ; ?></div>
       </div>
     </div>
     <!--footer end -->
 
    </div><!--center end -->
 </div><!-- end page_wrapper-->
+
 
 <?php if($page['float_qq']): ?>
 <div class="float_qq"><?php print render($page['float_qq']);?></div>
@@ -122,23 +140,6 @@ jQuery("#main-menu .root_menu li:last a").click(function(){
 </script>
 
 <script language="javascript">
-var m=0;
-function show_box(sid){
-  //manin menu show 
-  if(jQuery('.menu_des').css('display')=='none'){
-    jQuery('.all_menu').css('margin-left',(1-sid)*990);
-  }else{
-    jQuery('.all_menu').animate({marginLeft: (1-sid)*990}, 500);
-  }
-  if(m==sid){
-    jQuery('.menu_des').slideUp();
-    m=0;
-  }else{
-    m=sid;
-    jQuery('.menu_des').slideDown();
-  }
-
-}
 jQuery('.root_menu li').click(function(){
   jQuery('.root_menu li').removeClass('active');
   jQuery(this).addClass('active');
@@ -152,6 +153,7 @@ jQuery('#page_top').mouseleave(function(){
   setTimeout(setLeaveMouse,1000);
 
 });
+
 
 jQuery("#edit-submitted-you-full-name").val("Your full name");
 jQuery("#edit-submitted-you-email").val("name@example.com");
@@ -174,7 +176,7 @@ jQuery("#edit-submitted-you-full-name").blur(function(){
   if(jQuery(this).val() == "") {jQuery(this).val("Your full name");jQuery(this).css("color","#999");}
 });
 jQuery("#edit-submitted-you-email").blur(function(){
-  if(jQuery(this).val() == "") {jQuery(this).val("name@example.com");jQuery(this).css("color","#999 !imp");}
+  if(jQuery(this).val() == "") {jQuery(this).val("name@example.com");jQuery(this).css("color","#999");}
 });
 jQuery("#edit-submitted-body").blur(function(){
   if(jQuery(this).val() == ""){ jQuery(this).val("Your question");jQuery(this).css("color","#999");}
@@ -192,13 +194,13 @@ jQuery(".webform-client-form .form-submit").click(function(){
     return false;
   }
 
-}
-if(!chvarchar.test(fbody) || fbody=="Your question" ){
-  alert("Please input your question.");
-  return false;
-}
-    });
-jQuery("#block-locale-language .content").append('<div id="language_arrow"><a><img src="sites/all/themes/yeaoh_website/images/flag_english.png"/><span>English</span></a></div>');
+  if(!chvarchar.test(fbody) || fbody=="Your question" ){
+    alert("Please input your question.");
+    return false;
+  }
+
+});
+jQuery("#block-locale-language .content").append('<div id="language_arrow"><a href="####"><img src="http://dev.yeaoh.com/sites/all/themes/yeaoh_website/images/flag_english.png"/><span>English</span></a></div>');
 jQuery("#language_arrow").click(function(){
   jQuery(".language-switcher-locale-session").animate({height:"0"},500);
 });
@@ -212,29 +214,9 @@ jQuery(document).click(function(){
 var start_num=str_url.indexOf("?language=zh-hans");
 if(start_num>0)
 {
-  jQuery("#language_arrow img").attr("src","sites/all/themes/yeaoh_website/images/zh-hans.png");
+  jQuery("#language_arrow img").attr("src","http://dev.yeaoh.com/sites/all/themes/yeaoh_website/images/zh-hans.png");
   jQuery("#language_arrow span").html("中文简体");
 }
-jQuery(".zh-hans a").attr("href","/?language=zh-hans");
+
 -->
-  </script>
-<script>
-
-//fancybox 
-
-jQuery('.fancybox').html('View Screenshot');
-
-jQuery('.view-example .view-grouping-content').mouseover(function(){
-
-  jQuery(this).css({'opacity':'100'});
-  jQuery(this).parent().css({'box-shadow':'0 0 3px #555555'});
-
-});
-jQuery('.view-example .view-grouping-content').mouseout(function(){
-
-  jQuery(this).css({'opacity':'0'});
-  jQuery(this).parent().css({'box-shadow':'none'});
-
-});
-
-</script>	
+  </script>	
