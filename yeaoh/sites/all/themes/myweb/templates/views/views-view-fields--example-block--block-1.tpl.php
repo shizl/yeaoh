eaa61464af979a0dd1edb04a9499f6d1c6e4dd48
neo <kaiyuan.an@yeaoh.net>
2014-06-10 15:00:51 +0800

    front expleam

--- yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--example-block--block-1.tpl.php
+++ yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--example-block--block-1.tpl.php
@@ -4,8 +4,23 @@
 </div>
 </div></div>
 <div class="rightSlide">
-<div class="body">
+
+
+<div class="title">Urban Studio</div>
+
+
+<div class="brief">
+
+<p>Urban Studio are a New Plymouth based Real Estate company. </p>
+<h3>Features of Website:</h3>
 <?php print $fields['body']->content;?>
+<p></p>
+
 </div>
+
+<div class="link"><i class="fa fa-external-link-square"></i> <a href="<?php print $fields['field_ex_link']->content;?>" rel="nofollow" target="_blank">
+<?php print $fields['field_ex_link']->content;?>
+</a></div>
+
 </div>
 
