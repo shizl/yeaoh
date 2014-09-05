e7ee14c63d3b383c5fe1af3584ca6dde7230b56d
yeaoh <support@yeaoh.com>
2014-09-05 15:52:27 +0800

    update websites link

--- yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--example-block--block-1.tpl.php
+++ yeaoh/sites/all/themes/myweb/templates/views/views-view-fields--example-block--block-1.tpl.php
@@ -12,13 +12,19 @@
 <div class="brief">
 <?php print $fields['body_1']->content;?>
 <h3>Features of Website:</h3>
-<?php print $fields['body']->content;?>
+<?php 
+print $fields['body']->content;
+?>
 <p></p>
 
 </div>
 
 <div class="link"><i class="fa fa-external-link-square"></i> <a href="<?php print $fields['field_ex_link']->content;?>" rel="nofollow" target="_blank">
-<?php print $fields['field_ex_link']->content;?>
+<?php
+//print $fields['field_ex_link']->content;
+print t('View Website');
+
+?>
 </a></div>
 
 </div>
